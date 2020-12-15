0bfa5ef8133cea88a969b708e8e96fbfbf2daf2b
ben2724 <55649670+ben2724@users.noreply.github.com>
2020-12-15 20:42:40 +0000

    Add files via upload
    attempted dark mode button using JavaScript as well as 2nd attempt at page transition. small additions made.

---
+++ website/javascript.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//select background and foreground for a fade in image transition.
+const space = document.querySelector(".space");
+const slider = document.querySelector(".slider");
+
+const switcher = document.querySelector('.button');
+
+//attempt to create animation
+const anim = new timelinemax();
+anim.FromTo(space,2, {height: "0%"}, {height: "100%"})
+
+
+//darkmode attempt
+switcher.addEventListener('click', function() {
+
+    document.body.classList.toggle('dark')  } );
+
+    var mode = document.body.mode;
+    if(mode == "lightmode") {
+        this.textContent = "dark";
+    }
+    else {
+        this.textContent = "light";
+    }
